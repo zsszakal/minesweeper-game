f7891d1a86a8f4d6933a97eec7dab6e22ccf9982
zsszakal <zsszakal@outlook.com>
2022-04-25 21:03:50 +0200

    Add buttons and click actions

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 from tkinter import *
+from cell import Cell
 import settings
 import utils
+
 
 root = Tk()
 # Override the settings of the window
@@ -36,6 +38,36 @@ center_frame.place(
     x=utils.width_prct(25),
     y=utils.height_prct(25)
 )
+
+"""
+btn1 = Button(
+    center_frame,
+    bg='blue',
+    text='First Button' 
+)
+btn1.place(x=0, y=0)
+
+
+c1 = Cell()
+c1.create_btn_object(center_frame)
+c1.cell_btn_object.grid(
+    column=0, row=0
+)
+
+c2 = Cell()
+c2.create_btn_object(center_frame)
+c2.cell_btn_object.grid(
+    column=1, row=0
+)
+"""
+
+for x in range(settings.GRID_SIZE):
+    for y in range(settings.GRID_SIZE):
+        c = Cell()
+        c.create_btn_object(center_frame)
+        c.cell_btn_object.grid(
+            column=x, row=y
+        )
 
 #Run the window
 root.mainloop()
