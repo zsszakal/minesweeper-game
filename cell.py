cd9d8113b628ea6236518db677a56c77a6429b4b
zsszakal <zsszakal@outlook.com>
2022-04-25 22:52:08 +0200

    Implement logic for surrounded cells

--- cell.py
+++ cell.py
@@ -17,16 +17,56 @@ class Cell:
         btn = Button(
             location,
             width=12,
-            height=4,
-            text=f"{self.x}, {self.y}"
+            height=4
         )
         btn.bind('<Button-1>', self.left_click_actions) # Left click
         btn.bind('<Button-3>', self.right_click_actions) # Right click
         self.cell_btn_object = btn
 
     def left_click_actions(self, event):
-        print(event)
-        print("I am left clicked!")
+        if self.is_mine:
+            self.show_mine()
+        else:
+            self.show_cell()
+
+    def get_cell_by_axis(self, x, y):
+        # Return a cell object based on the values of x, y
+        for cell in Cell.all:
+            if cell.x == x and cell.y == y:
+                return cell
+
+    @property
+    def surrounded_cells(self):
+        cells = [
+            self.get_cell_by_axis(self.x - 1, self.y - 1),
+            self.get_cell_by_axis(self.x - 1, self.y),
+            self.get_cell_by_axis(self.x - 1, self.y + 1),
+            self.get_cell_by_axis(self.x, self.y - 1),
+            self.get_cell_by_axis(self.x + 1, self.y - 1),
+            self.get_cell_by_axis(self.x + 1, self.y),
+            self.get_cell_by_axis(self.x + 1, self.y + 1),
+            self.get_cell_by_axis(self.x, self.y + 1)
+        ]
+
+        # Eliminate None values in surrounded_cells
+        cells = [cell for cell in cells if cell is not None]
+        return cells
+
+    #Count the mines in the surrounded cells when a cell is clicked
+    @property
+    def surrounded_cells_mines_length(self):
+        counter = 0
+        for cell in self.surrounded_cells:
+            if cell.is_mine:
+                counter += 1
+        return counter
+
+    def show_cell(self):
+        self.cell_btn_object.configure(text=self.surrounded_cells_mines_length)
+
+    def show_mine(self):
+        # A logic to interrupt the game and display a message that player lost
+        self.cell_btn_object.configure(bg='red')
 
     def right_click_actions(self, event):
         print(event)
